dd6e54bcd57704f552cf47b12d202b3021d96f9e
maymoon54 <69791357+maymoon54@users.noreply.github.com>
2020-08-27 23:48:55 +0900

    Update 중국음식주문하기.java

--- java09/src/배열정리/중국음식주문하기.java
+++ java09/src/배열정리/중국음식주문하기.java
@@ -12,80 +12,96 @@ import java.awt.event.ActionEvent;
 
 public class 중국음식주문하기 {
 	private static JTextField count;
-	
+
 	static int countfood = 0; // 주문수량 체크
+	static int sum = 0; // 금액합산
 
 	public static void main(String[] args) {
+
+		// 음식이름, 음식이미지
+		String[] titles = { "짬뽕", "우동", "짜장" };
+		String[] imges = { "짬뽕.jpg", "우동.jpg", "짜장면.jpg" };
+
+		// 전체 레이아웃
+		JFrame f = new JFrame();
+		f.setSize(500, 500);
+		f.getContentPane().setLayout(null);
+
+		// 음식이미지가 들어갈곳 : foodimg
+		JLabel foodimg = new JLabel("New label");
+		foodimg.setIcon(new ImageIcon(imges[1]));
+		foodimg.setBounds(36, 82, 408, 285);
+		f.getContentPane().add(foodimg);
+
+		// 음식수량 들어갈곳: count
+		JLabel lblNewLabel = new JLabel("개수입력");
+		lblNewLabel.setBounds(290, 16, 54, 25);
+		f.getContentPane().add(lblNewLabel);
 		
-		//음식이름, 음식이미지
-		String[] titles = {"짬뽕","우동","짜장"};
-		String[] imges = {"짬뽕.jpg","우동.jpg","짜장면.jpg"};
-		
-		
-		
-		 JFrame f = new JFrame();
-		 f.setSize(500,500);
-		 f.getContentPane().setLayout(null);
-		 
-		 JLabel foodimg = new JLabel("New label");
-		 foodimg.setIcon(new ImageIcon(imges[1]));
-		 foodimg.setBounds(36, 82, 408, 285);
-		 f.getContentPane().add(foodimg);
-		 
-		 JButton btnNewButton = new JButton(titles[0]);
-		 btnNewButton.addActionListener(new ActionListener() {
-		 	public void actionPerformed(ActionEvent e) {
-		 	countfood++;
-		 	ImageIcon icon = new ImageIcon(imges[0]);
-		 	foodimg.setIcon(icon);
-		 	}
-		 });
-		 btnNewButton.setBounds(12, 10, 70, 36);
-		 f.getContentPane().add(btnNewButton);
-		 
-		 JButton btnNewButton_1 = new JButton(titles[1]);
-		 btnNewButton_1.addActionListener(new ActionListener() {
-		 	public void actionPerformed(ActionEvent e) {
-		 		ImageIcon icon = new ImageIcon(imges[1]);
-			 	foodimg.setIcon(icon);
-		 		
-		 	}
-		 });
-		 btnNewButton_1.setBounds(94, 10, 76, 36);
-		 f.getContentPane().add(btnNewButton_1);
-		 
-		 JButton btnNewButton_2 = new JButton(titles[2]);
-		 btnNewButton_2.addActionListener(new ActionListener() {
-		 	public void actionPerformed(ActionEvent e) {
-		 		ImageIcon icon = new ImageIcon(imges[2]);
-			 	foodimg.setIcon(icon);
-		 	}
-		 });
-		 btnNewButton_2.setBounds(182, 10, 76, 36);
-		 f.getContentPane().add(btnNewButton_2);
-		 
-		 JLabel lblNewLabel = new JLabel("개수입력");
-		 lblNewLabel.setBounds(290, 16, 54, 25);
-		 f.getContentPane().add(lblNewLabel);
-		 
-		 count = new JTextField();
-		 
-		 count.setBounds(356, 18, 116, 21);
-		 f.getContentPane().add(count);
-		 count.setColumns(10);
-		 
-		 
-		 
-		 JLabel lblNewLabel_2 = new JLabel("지불할 총 금액");
-		 lblNewLabel_2.setFont(new Font("바탕체", Font.PLAIN, 20));
-		 lblNewLabel_2.setBounds(46, 401, 160, 36);
-		 f.getContentPane().add(lblNewLabel_2);
-		 
-		 JLabel money = new JLabel("15000원");
-		 money.setFont(new Font("바탕체", Font.PLAIN, 20));
-		 money.setBounds(278, 400, 194, 39);
-		 f.getContentPane().add(money);
-		 f.setVisible(true);
+		count = new JTextField();
+		count.setBounds(356, 18, 116, 21);
+		f.getContentPane().add(count);
+		count.setColumns(10);
+
+		// 금액 들어갈곳: money
+		JLabel lblNewLabel_2 = new JLabel("지불할 총 금액");
+		lblNewLabel_2.setFont(new Font("바탕체", Font.PLAIN, 20));
+		lblNewLabel_2.setBounds(46, 401, 160, 36);
+		f.getContentPane().add(lblNewLabel_2);
+
+		JLabel money = new JLabel("0원");
+		money.setFont(new Font("바탕체", Font.PLAIN, 20));
+		money.setBounds(278, 400, 194, 39);
+		f.getContentPane().add(money);
+
+		// 버튼1
+		JButton b1 = new JButton(titles[0]);
+		b1.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				ImageIcon icon = new ImageIcon(imges[0]);
+				foodimg.setIcon(icon);
+				countfood++; //버튼을 누를때마다 1씩 증가
+				count.setText(countfood + "개"); //count에 출력
+				sum = sum + 5000; //음식가격 누적합산
+				money.setText(sum+"원"); // money에 출력
+			}
+		});
+		b1.setBounds(12, 10, 70, 36);
+		f.getContentPane().add(b1);
+
+		// 버튼2
+		JButton b2 = new JButton(titles[1]);
+		b2.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				ImageIcon icon = new ImageIcon(imges[1]);
+				foodimg.setIcon(icon);
+				countfood++;
+				count.setText(countfood + "개");
+				sum = sum + 5000; 
+				money.setText(sum+"원"); 
+
+			}
+		});
+		// 버튼3
+		b2.setBounds(94, 10, 76, 36);
+		f.getContentPane().add(b2);
+
+		JButton b3 = new JButton(titles[2]);
+		b3.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				ImageIcon icon = new ImageIcon(imges[2]);
+				foodimg.setIcon(icon);
+				countfood++;
+				count.setText(countfood + "개");
+				sum = sum + 5000; 
+				money.setText(sum+"원"); 
+			}
+		});
+		b3.setBounds(182, 10, 76, 36);
+		f.getContentPane().add(b3);
+
+		//화면에 보이기
+		f.setVisible(true);
 
 	}
 }
